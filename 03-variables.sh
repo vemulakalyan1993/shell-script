67ae0191933deddc72c19bd1a72d1b3e2dbccbf6
VemulaKalyan <vemulakalyan1993>
2026-09-14 19:05:06 +0530

    convsation added

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# this is indicate the comment line in bash script
+
+PERSON1="Rahin"
+PERSON2="Robbert"
+
+echo " ${PERSON1} : Hi ${PERSON2}"
+echo "${PERSON2} : Hi ${PERSON1}"
+echo "${PERSON1} : How are you?"
+echo "${PERSON2} : I am fine, How are you?"

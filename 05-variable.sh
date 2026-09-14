bbf1888aa1905bf7623144f652eb15ac49f511d1
VemulaKalyan <vemulakalyan1993>
2026-09-14 20:35:34 +0530

    added

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 echo "Please enter a username:"
-read USERNAME
+read -s USERNAME
 echo "username entered: $USERNAME"
+echo "Please enter a password:"
+read -s PASSWORD
+echo "password entered: $PASSWORD"

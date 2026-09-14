8ed41871b8c5aa85cc9560f21a12f62934d70fd4
VemulaKalyan <vemulakalyan1993>
2026-09-14 19:09:41 +0530

    variable declaration

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,10 +2,10 @@
 
 # this is indicate the comment line in bash script
 
-PERSON1="Rahin"
-PERSON2="Robbert"
+PERSON1=Ramesh
+PERSON2=Rahim
 
-echo " ${PERSON1} : Hi ${PERSON2}"
-echo "${PERSON2} : Hi ${PERSON1}"
-echo "${PERSON1} : How are you?"
-echo "${PERSON2} : I am fine, How are you?"
+echo "$PERSON1 : Hi $PERSON2"
+echo "$PERSON2 : Hi $PERSON1"
+echo "$PERSON1 : How are you?"
+echo "$PERSON2 : I am fine, How are you?"

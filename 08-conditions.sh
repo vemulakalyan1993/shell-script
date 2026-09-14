a3dd4046970080cbe5adecdbbb953e68b86ff7b9
VemulaKalyan <vemulakalyan1993>
2026-09-14 22:42:21 +0530

    added

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -6,6 +6,4 @@ if [ $NUMBER -gt 100 ]; then
     echo "The number $NUMBER is greater than 100."
 elif [ $NUMBER -lt 0 ]; then
     echo "The number $NUMBER is less than 50."
-else
-    echo "The number $NUMBER is between 0 and 100."
 fi

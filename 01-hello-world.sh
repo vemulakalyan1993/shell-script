2a41a6d3777a89c3cd560ca1b13df49595fc06d4
VemulaKalyan <vemulakalyan1993>
2026-09-14 15:48:02 +0530

    becomoe a devops engineer

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,7 +2,4 @@
 
 # this is indicate the comment line in bash script
 
-echo "Hello World"
-echo "Hi world"
-echo "Hello World and hi world1"
-echo "Hello World and hi world2"
+echo "Hi All going to learn bash scripting to become a devops engineer"

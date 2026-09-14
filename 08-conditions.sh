5eb7a9853182b9e33b81593dd9a6d81f5b3a6a33
VemulaKalyan <vemulakalyan1993>
2026-09-14 22:41:14 +0530

    added

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -2,10 +2,10 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 0 ]; then
-    echo "The number $NUMBER is positive."
+if [ $NUMBER -gt 100 ]; then
+    echo "The number $NUMBER is greater than 100."
 elif [ $NUMBER -lt 0 ]; then
-    echo "The number $NUMBER is negative."
+    echo "The number $NUMBER is less than 50."
 else
-    echo "The number is zero."
+    echo "The number $NUMBER is between 0 and 100."
 fi

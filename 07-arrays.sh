2ed6c9993a83dc359f3254edc87643680fa86203
VemulaKalyan <vemulakalyan1993>
2026-09-14 22:13:42 +0530

    added

---
+++ 07-arrays.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MOVIES =(
+  "Inception"
+  "The Dark Knight"
+  "Interstellar"
+  "The Matrix"
+  "Pulp Fiction"
+)
+
+echo "List of Movies:"
+for MOVIE in "${MOVIES[@]}"; do
+  echo "- $MOVIE"
+done
